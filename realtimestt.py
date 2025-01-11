05c8b432be0ffc3c895a610e63b3169c5e874054
Zuhaib Khan <adnankhanmgdp@gmail.com>
2025-01-11 15:55:11 +0530

    master

--- realtimestt.py
+++ realtimestt.py
@@ -3,27 +3,17 @@ import queue
 import re
 import sys
 import os
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']="./creds.json"
-
 from google.cloud import speech
-
 import pyaudio
 
-# Audio recording parameters
 RATE = 16000
-CHUNK = int(RATE / 10)  # 100ms
-
+CHUNK = int(RATE / 10)
 
 class MicrophoneStream:
-    """Opens a recording stream as a generator yielding the audio chunks."""
 
     def __init__(self: object, rate: int = RATE, chunk: int = CHUNK) -> None:
-        """The audio -- and generator -- is guaranteed to be on the main thread."""
         self._rate = rate
         self._chunk = chunk
-
-        # Create a thread-safe buffer of audio data
         self._buff = queue.Queue()
         self.closed = True
 
@@ -31,20 +21,14 @@ class MicrophoneStream:
         self._audio_interface = pyaudio.PyAudio()
         self._audio_stream = self._audio_interface.open(
             format=pyaudio.paInt16,
-            # The API currently only supports 1-channel (mono) audio
-            # https://goo.gl/z757pE
             channels=1,
             rate=self._rate,
             input=True,
             frames_per_buffer=self._chunk,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
             stream_callback=self._fill_buffer,
         )
 
         self.closed = False
-
         return self
 
     def __exit__(
@@ -53,12 +37,9 @@ class MicrophoneStream:
         value: object,
         traceback: object,
     ) -> None:
-        """Closes the stream, regardless of whether the connection was lost or not."""
         self._audio_stream.stop_stream()
         self._audio_stream.close()
         self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
         self._buff.put(None)
         self._audio_interface.terminate()
 
@@ -69,39 +50,15 @@ class MicrophoneStream:
         time_info: object,
         status_flags: object,
     ) -> object:
-        """Continuously collect data from the audio stream, into the buffer.
-
-        Args:
-            in_data: The audio data as a bytes object
-            frame_count: The number of frames captured
-            time_info: The time information
-            status_flags: The status flags
-
-        Returns:
-            The audio data as a bytes object
-        """
         self._buff.put(in_data)
         return None, pyaudio.paContinue
 
     def generator(self: object) -> object:
-        """Generates audio chunks from the stream of audio data in chunks.
-
-        Args:
-            self: The MicrophoneStream object
-
-        Returns:
-            A generator that outputs audio chunks.
-        """
         while not self.closed:
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
             chunk = self._buff.get()
             if chunk is None:
                 return
             data = [chunk]
-
-            # Now consume whatever other data's still buffered.
             while True:
                 try:
                     chunk = self._buff.get(block=False)
@@ -115,46 +72,14 @@ class MicrophoneStream:
 
 
 def listen_print_loop(responses: object) -> str:
-    """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-
-    Args:
-        responses: List of server responses
-
-    Returns:
-        The transcribed text.
-    """
     num_chars_printed = 0
     for response in responses:
         if not response.results:
             continue
-
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
         result = response.results[0]
         if not result.alternatives:
             continue
-
-        # Display the transcription of the top alternative.
         transcript = result.alternatives[0].transcript
-
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
         overwrite_chars = " " * (num_chars_printed - len(transcript))
 
         if not result.is_final:
@@ -165,58 +90,36 @@ def listen_print_loop(responses: object) -> str:
         else:
             # print("user-said: ",end="")
             # print(transcript + overwrite_chars)
-            
-            with open('query.txt','w') as f:
-                f.write(transcript+overwrite_chars)
-                f.close()
-            
-            exit(0)
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                print("Exiting..")
-                break
-
-            num_chars_printed = 0
-
+            # if re.search(r"\b(exit|quit)\b", transcript, re.I):
+            #     break
+            break
     return transcript
 
 
-def main() -> None:
-    """Transcribe speech from audio file."""
-    # See http://g.co/cloud/speech/docs/languages
-    # for a list of supported languages.
-    language_code = "en-IN" 
-
+def listen(credentials_path:str, model:str="latest_short", language_code:str="en-IN") -> str:
+    """
+    Args:
+    credentials_path: </path/to/credentials.json> file
+    model: View more models on GCP
+    rate: 16000 (default)
+    """
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']=credentials_path
     client = speech.SpeechClient()
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=RATE,
         language_code=language_code,
-        model='latest_short'
+        model=model
     )
-
     streaming_config = speech.StreamingRecognitionConfig(
         config=config, interim_results=True
     )
-    ch = 'y'
-    while ch == 'y':
-        with MicrophoneStream(RATE, CHUNK) as stream:
-            audio_generator = stream.generator()
-            requests = (
-                speech.StreamingRecognizeRequest(audio_content=content)
-                for content in audio_generator
-            )
-
-            responses = client.streaming_recognize(streaming_config, requests)
-
-            # Now, put the transcription responses to use.
-            query=listen_print_loop(responses)
-            print(f"user said: {query}")
-            ch = input("do you want to run it again? (y/n)")
-    
-
-
-if __name__ == "__main__":
-    main()
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+        responses = client.streaming_recognize(streaming_config, requests)
+        query = listen_print_loop(responses)
+        return query
--- test-realtimescript.py
+++ test-realtimescript.py
@@ -1,11 +1,12 @@
-# import realtimestt
+import realtimestt
 import os
 
 ch = 'y'
 while ch == 'y':
-    os.system("python3 realtimestt.py")
-    with open('query.txt','r') as f:
-        content = f.read()
-        f.close()
+    # os.system("python3 realtimestt.py")
+    content = realtimestt.listen(credentials_path="creds.json", model="latest_short", language_code='en-IN')
+    # with open('query.txt','r') as f:
+    #     content = f.read()
+    #     f.close()
     print(f"user-said: {content}")
     ch = input("Do you want do it again? (y/n): ")
